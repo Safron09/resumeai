d28a701088043d14143af8f64e725db5760f6773
Yuriy Safronnynov <y.safronnikov@gmail.com>
2026-03-13 12:28:12 -0400

    Loggs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -176,3 +176,29 @@ _frontend_dist = BASE_DIR / 'frontend' / 'dist'
 if _frontend_dist.exists():
     WHITENOISE_ROOT = _frontend_dist
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
